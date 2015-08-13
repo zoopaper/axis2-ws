231649592c792b645d97697beeae79d854297898
krisjin <krisibm@163.com>
2015-08-14 07:57:15 +0800

    client

---
+++ src/main/java/net/service/client/SmsClient.java
@@ -0,0 +1,46 @@
+package net.service.client;
+
+import org.apache.axis2.AxisFault;
+
+import java.io.UnsupportedEncodingException;
+import java.rmi.RemoteException;
+
+/**
+ * wsdl2java 工具生成
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/7/22
+ * Time: 13:48
+ */
+public class SmsClient {
+
+    public static void main(String[] args) {
+        try {
+            SmsFeedbackServiceStub stub = new SmsFeedbackServiceStub();
+
+            SmsFeedbackServiceStub.ResultFeedback resultFeedback = new SmsFeedbackServiceStub.ResultFeedback();
+
+            SmsFeedbackServiceStub.ResultFeedbackEvt resultFeedbackEvt = new SmsFeedbackServiceStub.ResultFeedbackEvt();
+
+
+            resultFeedbackEvt.setMSISDN("18810489320");
+            resultFeedbackEvt.setRescode("900000");
+            resultFeedbackEvt.setResdesc("msg desc");
+            resultFeedbackEvt.setRevdate("20150813175812");
+//            resultFeedbackEvt.setMsg(new String("暗香@526@1200@908@order@222".getBytes(), "UTF-8"));
+            resultFeedbackEvt.setMsg(new String("明科@111@1200@908@mm@222".getBytes(), "UTF-8"));
+
+
+            resultFeedback.setEvent(resultFeedbackEvt);
+            SmsFeedbackServiceStub.Response response = stub.resultFeedback(resultFeedback).get_return();
+            String desc = response.getReturn_desc();
+            System.out.println(desc);
+        } catch (AxisFault axisFault) {
+            axisFault.printStackTrace();
+        } catch (RemoteException re) {
+            re.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
